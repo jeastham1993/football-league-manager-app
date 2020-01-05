17db598d5c45871773b59762fe3a16ffff2b1027
James Eastham <j.eastham1993@gmail.com>
2020-01-05 21:23:54 +0000

    [FEATURE] Add player struct and add player to team methods

--- src/team-service/cmd/team.go
+++ src/team-service/cmd/team.go
@@ -6,8 +6,15 @@ import (
 
 // Team is the central class in the domain model.
 type Team struct {
-	ID   string `json:"id"`
-	Name string `json:"teamName"`
+	ID      string    `json:"id"`
+	Name    string    `json:"teamName"`
+	Players []*Player `json:"players"`
+}
+
+// Player holds data for all the players that a team has
+type Player struct {
+	Name     string `json:"name"`
+	Position string `json:"position"`
 }
 
 // TeamRepository is an interface for the team database interactions
@@ -20,7 +27,27 @@ type TeamRepository interface {
 func NewTeam(name string) *Team {
 	newID := uuid.Must(uuid.NewV4()).String()
 	return &Team{
-		ID:   newID,
-		Name: name,
+		ID:      newID,
+		Name:    name,
+		Players: []*Player{},
 	}
+}
+
+// AddPlayerToTeam adds a new player to the specified team, if the player exists the existing player is returned.
+func (team *Team) AddPlayerToTeam(playerName string, position string) (*Player, error) {
+
+	for _, v := range team.Players {
+		if v.Name == playerName {
+			return v, nil
+		}
+	}
+
+	player := &Player{
+		Name:     playerName,
+		Position: position,
+	}
+
+	team.Players = append(team.Players, player)
+
+	return player, nil
 }
--- src/team-service/cmd/team_test.go
+++ src/team-service/cmd/team_test.go
@@ -19,3 +19,25 @@ func TestCreateNewTeamNameSet(t *testing.T) {
 		t.Fatalf("Team name has not been set")
 	}
 }
+
+func TestAddPlayerToTeam(t *testing.T) {
+	team := NewTeam("My test team")
+
+	team.AddPlayerToTeam("James", "GK")
+
+	if team.Players[0] == nil {
+		t.Fatalf("Player has not been added correctly")
+	}
+}
+
+func TestAddDuplicatePlayerToTeam(t *testing.T) {
+	team := NewTeam("My test team")
+
+	team.AddPlayerToTeam("James Eastham", "GK")
+	team.AddPlayerToTeam("James Testing", "GK")
+	team.AddPlayerToTeam("James Eastham", "GK")
+
+	if len(team.Players) == 3 {
+		t.Fatalf("Duplicate player has been added")
+	}
+}
