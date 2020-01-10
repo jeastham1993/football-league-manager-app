860c8abec3897444913d8045f854a293472a5097
James Eastham <j.eastham1993@gmail.com>
2020-01-10 23:03:02 +0000

    [FEATURE] Add inmem team repository

---
+++ src/team-service/interfaces/repositories.go
@@ -0,0 +1,41 @@
+package interfaces
+
+import (
+	"sync"
+
+	"github.com/jeastham1993/football-league-manager-app/src/team-service/domain"
+	"github.com/rs/xid"
+)
+
+// InMemTeamRepo manages in memory storage of teams.
+type InMemTeamRepo struct {
+	mtx   sync.RWMutex
+	teams map[string]*domain.Team
+}
+
+// NewInMemTeamRepo creates a new InMemTeamRepo
+func NewInMemTeamRepo() *InMemTeamRepo {
+	return &InMemTeamRepo{
+		teams: make(map[string]*domain.Team),
+	}
+}
+
+// Store creates a new team in the in memory storage.
+func (r *InMemTeamRepo) Store(team *domain.Team) string {
+	team.ID = xid.New().String()
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+	r.teams[team.ID] = team
+	return team.ID
+}
+
+// FindByID gets a team from the in memory store.
+func (r *InMemTeamRepo) FindByID(teamID string) *domain.Team {
+	r.mtx.RLock()
+	defer r.mtx.RUnlock()
+	if val, ok := r.teams[teamID]; ok {
+		return val
+	}
+
+	return nil
+}
